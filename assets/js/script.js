ac196eaf46fceaa05b02cf7502ff090c7aec2c76
Jake Pasternak <jdp.pasternak@gmail.com>
2021-12-02 16:20:47 -1000

    Set up collections for entries

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+/* 
+    Entry:
+    <a class="collection-item row d-flex justify-content-between">
+        <span class="col s12 m4 left-align" data-description data-entryid="1">
+            Description
+        </span>
+        <span class="col s12 m4 right-align" data-amount data-entryid="1">
+            $0.00
+        </span>
+    </a>
+*/
